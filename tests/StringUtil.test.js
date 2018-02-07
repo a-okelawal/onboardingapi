54db69d521971ab0de8825b60b1b31d3b0b353e6
Saint-lawal <abiso.lawal@gmail.com>
2018-02-07 16:27:40 +0100

    - Add tests for Token Utility

--- tests/StringUtil.test.js
+++ tests/StringUtil.test.js
@@ -1,11 +1,14 @@
+import bcrypt from 'bcrypt';
+import sinon from 'sinon';
 import should from 'should';
 
 import StringUtil from '../app/shared/StringUtil';
 
-var invalidEmails, validEmails;
+let invalidEmails, invalidNumbers, validEmails, validNumbers;
+let password;
 
 describe('String Utility', () => {
-  describe('Email Utility', () => {
+  describe('Email Validation', () => {
     before(() => {
       invalidEmails = [
         '@test.com',
@@ -30,6 +33,56 @@ describe('String Utility', () => {
     it('should return true for valid emails', () => {
       validEmails.forEach(element => {
         StringUtil.isEmailValid(element).should.equal(true);
+      });
+    });
+  });
+
+  describe('Phone Validation', () => {
+    before(() => {
+      invalidNumbers = [
+        '+2345558393884',
+        '984984984',
+        '000000000'
+      ];
+
+      validNumbers = [
+        '08022222222',
+        '09030203055',
+        '07145366666'
+
+      ];
+    });
+
+    it('should return false for invalid emails', () => {
+      invalidNumbers.forEach(element => {
+        StringUtil.isPhoneValid(element).should.equal(false);
+      });
+    });
+
+    it('should return true for valid emails', () => {
+      validNumbers.forEach(element => {
+        StringUtil.isPhoneValid(element).should.equal(true);
+      });
+    });
+  });
+
+  describe('Hash Password', () => {
+    before(function() {
+      sinon.stub(bcrypt, 'hashSync').returns('G4HG3H4G3H4G3J4');
+    });
+    
+    after(function() {
+      bcrypt.hashSync.restore();
+    });
+
+    it('should return hashed password', () => {
+      password = StringUtil.hashPassword();
+      password.should.equal('G4HG3H4G3H4G3J4');
+    });
+
+    it('should return true for valid emails', () => {
+      validNumbers.forEach(element => {
+        StringUtil.isPhoneValid(element).should.equal(true);
       });
     });
   });
