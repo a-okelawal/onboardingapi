3a975f86707eb2135f7c75b1bf4ace6f379e45fd
Saint-lawal <abiso.lawal@gmail.com>
2018-02-07 16:28:04 +0100

    - Add Token Utility logic

--- app/shared/StringUtil.js
+++ app/shared/StringUtil.js
@@ -1,4 +1,24 @@
+import bcrypt from 'bcrypt';
+
 export default class StringUtil {
+  /**
+   * Compare Passwords
+   * @param {*} password 
+   * @param {*} hash 
+   */
+  static comparePasswords(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  }
+  
+  /**
+   * Function to hash password
+   * @param {*} password 
+   */
+  static hashPassword(password) {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  }
+
   /**
    * Test if email is valid
    * @param {*} email 
